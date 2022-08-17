2e36cc409fd61a19263fe1dca5203b4b84e43103
Gustavo Alexandre <gustavo.nunes@pontotel.com.br>
2022-08-17 10:45:37 -0300

    Adicionando header e estrutura de loop nos posts

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello, world!"
-
+    user = {'username': 'Gustavo'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Bom dia pessoal!'
+        },
+        {
+            'author': {'username': 'Jorge'},
+            'body': 'Hoje está frio :D'
+        }
+    ]
+    return  render_template('index.html', title='Página inicial', user=user, posts=posts)
 
